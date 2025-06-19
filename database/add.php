6725515b77872098f688bb4e0aeb120d6e725f52
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-06-19 14:59:51 +0800

    working update

--- database/add.php
+++ database/add.php
@@ -22,5 +22,6 @@
     <label>Title: </label><br/>
     <input type="text" name="title"><br/><br/>
     <label>Content</label><br/>
+    <textarea name="content"></textarea><br/><br/>
     <input type="submit" value="Add Article">
 </form>
