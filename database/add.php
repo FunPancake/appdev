f5accbcb6c14087e9f2bdea1c6f7ee44df8a122c
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-06-19 14:15:26 +0800

    added content

--- database/add.php
+++ database/add.php
@@ -1,1 +1,26 @@
-sad
+<?php 
+    include 'config.php';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        //Get the Articles
+
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+
+        //inster the article into the database
+        $query = "insert into articles (title, content) values ('$title', '$content')";
+
+        mysqli_query($conn, $query);
+
+        // Redirect to the homepag after adding
+        header("Location:index.php");
+        exit();
+    }
+?>
+
+<form method="post" action="">
+    <label>Title: </label><br/>
+    <input type="text" name="title"><br/><br/>
+    <label>Content</label><br/>
+    <input type="submit" value="Add Article">
+</form>
